713f01d96d9dcc88cabbeca1c86ad6dabd915d19
Sugan Selvam <sugan170@gmail.com>
2020-09-01 12:54:47 +0530

    chnages

--- index.js
+++ index.js
@@ -1,65 +1,57 @@
 
+  
+
 var app = require("express")();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-
-
 const bcrypt = require("bcrypt")
-
-
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(bodyParser.json());
 app.use(cors());
 
-
 const port = process.env.PORT || 4040
-
-
-
 const mongodb = require("mongodb");
 const url =  "mongodb+srv://admin:admin@login.7aoxw.mongodb.net/Login?retryWrites=true&w=majority";
 
 
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
+io.on("connection", socket => {
+
+  socket.on("joinRoom", (roomID, userID) => {
+    console.log(roomID, userID)
+    socket.join(roomID)
+    socket.to(roomID).broadcast.emit("userConnected",userID)
+  } )
+
+  socket.on("disconnect",(roomID)=>{
+    socket.to(roomID).broadcast.emit("userDisconnected",userID)
   })
+
 })
 
 
 app.post("/", async (req, res) => {
 
-  console.log(req.body)
-
   if(req.body.SubmitType === "Login"){
 
     try {
 
-
+      //Creating a client from MongoDB URL & connecting it to it's Collection
       let client = await mongodb.connect(url);
       let db = client.db("login");
   
-
+      //getting user Data by finding it in the collection
       let dataUser = await db.collection("userLogin").find({ name: req.body.name }).toArray();
       await client.close();
 
       bcrypt.compare(req.body.password, dataUser[0].password, function(err, result) {
-          console.log("Result",result)          
           res.send({result,dataUser}); 
         });
 
-
-      console.log("User Data :",dataUser);
-      console.log(req.body);
   
-
+      //Error Handling
     } catch (err) {
       res.status(500).send('Something broke!')
     }
@@ -95,7 +87,7 @@ app.post("/", async (req, res) => {
 });
 
 
-
+//opening Port on 4040 or Heroku's Port
 http.listen(port, () => {
   console.log("listening on : ",port);
 });
